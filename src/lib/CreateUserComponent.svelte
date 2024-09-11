14c043111e44464e488a818f0e2374c6f1f85f84
602822 <89077126+602822@users.noreply.github.com>
2024-09-11 11:56:34 +0200

    now able to create user using the frontend

--- src/lib/CreateUserComponent.svelte
+++ src/lib/CreateUserComponent.svelte
@@ -1,4 +1,25 @@
 <script>
+    let username;
+    let email;
+
+    function createNewUser() {
+        fetch("http://localhost:8080/users", {
+            method:"POST",
+            body: JSON.stringify(
+               {username: username, email: email}
+            ), headers: {
+                "Content-Type":"application/json"
+            }
+        }).then((response) => {
+            if(response.status === 201) {
+                alert("User created successfully");
+                username = "";
+                email = "";
+            }
+        }).catch((error) => {
+            alert(error.message)
+        })
+    }
 
 </script>
 
@@ -6,14 +27,14 @@
 
 </style>
 
-<form>
+<form on:submit|preventDefault={createNewUser}>
     <div>
         <label>Username: </label>
-        <input type="text" id="username" name="username">
+        <input type="text" id="username" bind:value={username}>
     </div>
     <div>
         <label>Email: </label>
-        <input type="text" id="email" name="email">
+        <input type="text" id="email" bind:value={email}>
     </div>
-    <button>Create user</button>
+    <button type="submit">Create user</button>
 </form>
