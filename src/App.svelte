9114680817d92c5e4569e635c2fdb7e264f8986d
602822 <89077126+602822@users.noreply.github.com>
2024-09-11 11:04:03 +0200

    init svelte frontend and started on first component

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+
+  import CreateUserComponent from "./lib/CreateUserComponent.svelte";
+</script>
+
+<main>
+  <CreateUserComponent> </CreateUserComponent>
+</main>
+
+<style>
+
+</style>
---
+++ src/lib/CreateUserComponent.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<style>
+
+</style>
+
+<form>
+    <div>
+        <label>Username: </label>
+        <input type="text" id="username" name="username">
+    </div>
+    <div>
+        <label>Email: </label>
+        <input type="text" id="email" name="email">
+    </div>
+    <button>Create user</button>
+</form>
