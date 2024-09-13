f23e9cb2df2e408b6ec46bcc5fb3d6919ec99892
602822 <89077126+602822@users.noreply.github.com>
2024-09-13 11:26:02 +0200

    added a home component

--- src/App.svelte
+++ src/App.svelte
@@ -2,23 +2,14 @@
     import CreateUserComponent from "./lib/CreateUserComponent.svelte";
     import {Router, Route, Link} from "svelte-routing";
     import CreatePollComponent from "./lib/CreatePollComponent.svelte";
+    import HomeComponent from "./lib/HomeComponent.svelte";
 </script>
 
 <main>
   <Router>
-    <nav>
-        <ul>
-
-            <li>
-                <Link to="/users">Create User</Link>
-            </li>
-            <li>
-                <Link to="/polls">Create Poll</Link>
-            </li>
-        </ul>
-    </nav>
         <Route path="/users" component="{CreateUserComponent}"></Route>
         <Route path="/polls" component="{CreatePollComponent}"></Route>
+        <Route path="/" component="{HomeComponent}"></Route>
     </Router>
 
 
---
+++ src/lib/HomeComponent.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<style>
+div {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+    height: 100vh;
+}
+</style>
+
+<div>
+    <h1>Welcome!</h1>
+    <h2> Available endpoints: /users and /polls</h2>
+</div>
