f407234e8011573603cbdee100ab88da73523bca
602822 <89077126+602822@users.noreply.github.com>
2024-09-15 18:27:51 +0200

    homepage visual changes

--- src/lib/HomeComponent.svelte
+++ src/lib/HomeComponent.svelte
@@ -3,16 +3,27 @@
 </script>
 
 <style>
-div {
-    display: flex;
-    flex-direction: column;
-    justify-content: start;
-    align-items: center;
-    height: 100vh;
-}
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: start;
+        align-items: center;
+        height: 100vh;
+    }
 </style>
 
 <div>
     <h1>Welcome!</h1>
-    <h2> Available endpoints: /users and /polls/:username and /polls/:username/votes</h2>
+    <h2> Available endpoints: </h2>
+    <ul>
+        <li>
+            <h3> /users </h3>
+        </li>
+        <li>
+            <h3> /polls/:username </h3>
+        </li>
+        <li>
+            <h3> /polls/:username/votes </h3>
+        </li>
+    </ul>
 </div>
